6a4740fec076614145a8e8ddf840b2e153a8a880
lazar <lazar.trifunovic@boopro.tech>
2024-07-17 13:14:20 +0200

    feature(structure):Created variables sass file, fixed structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import "./ui/styles/configurator.scss";
+
 function App() {
   return (
     <main className="configurator">
-      <article className="configurator__left">
-        <h1>Deking zona</h1>
-      </article>
-      <article className="configurator__right"></article>
+      <article className="configurator__left">3D CANVAS HERE</article>
+      <article className="configurator__right">FORM HERE</article>
     </main>
   );
 }
