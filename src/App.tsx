6ace6d08a8bfa9a6e036ad15986d46b1e6609020
lazar <lazar.trifunovic@boopro.tech>
2024-07-18 15:11:41 +0200

    feature(threeJS):Added ThreeJS FIber, Drei, 3D Models

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import AppCanvas from "./ui/components/containers/AppCanvas.container";
 import "./ui/styles/configurator.scss";
 
 function App() {
   return (
     <main className="configurator">
-      <article className="configurator__left">3D CANVAS HERE</article>
+      <article className="configurator__left">
+        <AppCanvas />
+      </article>
       <article className="configurator__right">FORM HERE</article>
     </main>
   );
---
+++ src/ui/components/containers/AppCanvas.container.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useRef, useState } from "react";
+import { easing } from "maath";
+import { Center, Sky } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+import FenceTest from "../models/FenceTest2";
+
+import "../../styles/containers/appCanvas.container.scss";
+
+export default function AppCanvas() {
+  return (
+    <div className="three-canvas">
+      <Canvas>
+        <Lights />
+
+        <CameraRig>
+          <Center>
+            <FenceTest />
+          </Center>
+        </CameraRig>
+
+        {/* <OrbitControls  /> */}
+      </Canvas>
+    </div>
+  );
+}
+
+function Lights() {
+  return (
+    <>
+      <Sky
+        azimuth={0}
+        sunPosition={1}
+        rayleigh={0.25}
+        inclination={0.6}
+        distance={500}
+      />
+
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[10, 10, 5]} />
+    </>
+  );
+}
+
+function CameraRig({ children }: { children: React.ReactNode }) {
+  const cameraRef = useRef<any>();
+
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useFrame((_, delta) => {
+    easing.dampE(
+      cameraRef.current.rotation,
+      [mousePosition.y / 7000, -mousePosition.x / 5000, 0],
+      0.5,
+      delta
+    );
+  });
+
+  return <group ref={cameraRef}>{children}</group>;
+}
