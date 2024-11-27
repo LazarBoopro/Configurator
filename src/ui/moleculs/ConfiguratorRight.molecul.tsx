1fdbfe3a7a7361f161cd291733d8b072cb31d4a0
Milica Jovanovic <milica.jovanovic@boopro.tech>
2024-11-27 14:55:10 +0100

    feat(contact): added contact button

--- src/ui/moleculs/ConfiguratorRight.molecul.tsx
+++ src/ui/moleculs/ConfiguratorRight.molecul.tsx
@@ -2,30 +2,44 @@ import { useValues } from "../../context/FormValuesContext";
 import { Button } from "../components/atoms/Button.atom";
 import Form from "../components/containers/Form.container";
 
-export default function ConfiguratorRight({
-  setPage,
-}: {
-  setPage: CallableFunction;
-}) {
-  const { values } = useValues();
+import email from "../../assets/email.svg";
+import tel from "../../assets/tel.svg";
 
-  return (
-    <article className="configurator__right">
-      <Form />
-      <div className="submit-buttons">
-        <Button
-          variant="primary"
-          onClick={() => {
-            setPage(1);
-          }}
-          disabled={values.length < 0.9}
-        >
-          Obračun
-        </Button>
-        <Button disabled variant="outline" onClick={() => {}}>
-          Zakažite izlazak na teren
-        </Button>
-      </div>
-    </article>
-  );
+export default function ConfiguratorRight({ setPage }: { setPage: CallableFunction }) {
+    const { values } = useValues();
+
+    return (
+        <article className="configurator__right">
+            <Form />
+            <div className="submit-buttons">
+                <Button
+                    variant="primary"
+                    onClick={() => {
+                        setPage(1);
+                    }}
+                    disabled={values.length < 0.9}
+                >
+                    Obračun
+                </Button>
+                <div className="schedule">
+                    Zakažite izlazak na teren
+                    <div style={{ display: "flex" }}>
+                        <a
+                            href="mailto:info@deking.com"
+                            // className="button outline mailto"
+                        >
+                            <img src={email} alt="mail" />
+                        </a>
+                        <a
+                            href="tel:+38162209207"
+                            // className="button outline tel"
+                        >
+                            <img src={tel} alt="tel" />
+                            {/* Zakažite izlazak na teren poziv */}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </article>
+    );
 }
