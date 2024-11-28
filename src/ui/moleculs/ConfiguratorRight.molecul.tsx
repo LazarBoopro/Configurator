644b102575ec62c1e218e46426230dc343bc7cc5
Milica Jovanovic <milica.jovanovic@boopro.tech>
2024-11-28 11:46:22 +0100

    code cleanup

--- src/ui/moleculs/ConfiguratorRight.molecul.tsx
+++ src/ui/moleculs/ConfiguratorRight.molecul.tsx
@@ -1,9 +1,7 @@
 import { useValues } from "../../context/FormValuesContext";
 import { Button } from "../components/atoms/Button.atom";
+import Contact from "../components/atoms/Contact.atom";
 import Form from "../components/containers/Form.container";
-
-import email from "../../assets/email.svg";
-import tel from "../../assets/tel.svg";
 
 export default function ConfiguratorRight({ setPage }: { setPage: CallableFunction }) {
     const { values } = useValues();
@@ -11,70 +9,8 @@ export default function ConfiguratorRight({ setPage }: { setPage: CallableFuncti
     return (
         <article className="configurator__right">
             <Form />
-            <div
-                className="submit-buttons"
-                style={{
-                    alignContent: "flex-start",
-                }}
-            >
-                <div
-                    className="schedule"
-                    style={{
-                        display: "flex",
-                        // justifyContent: "center",
-                        alignItems: "start",
-                        // alignContent: "flex-start!important",
-                        gap: ".5rem",
-                        width: "100%",
-                    }}
-                >
-                    <p
-                        style={{
-                            fontWeight: "800",
-                            textAlign: "left",
-                            justifyContent: "space-between",
-                            fontSize: "clamp(1.125rem, 3vw, 1.2rem)",
-                            marginRight: "auto",
-                        }}
-                    >
-                        Zakažite izlazak na teren
-                    </p>
-                    <div
-                        style={{ display: "flex", justifyContent: "flex-start", flexWrap: "wrap" }}
-                    >
-                        <a
-                            style={{
-                                padding: ".5rem 1rem",
-                                display: "flex",
-                                alignContent: "center",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                gap: ".5rem",
-                            }}
-                            href="mailto:info@deking.com"
-                            // className="button outline mailto"
-                        >
-                            <img style={{ width: "1.25rem" }} src={email} alt="mail" />
-                            <p>Posalji email</p>
-                        </a>
-                        <a
-                            href="tel:+38162209207"
-                            style={{
-                                padding: ".5rem 1rem",
-                                display: "flex",
-                                alignContent: "center",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                gap: ".5rem",
-                            }}
-                            // className="button outline tel"
-                        >
-                            <img style={{ width: "1.25rem" }} src={tel} alt="tel" />
-                            <p>Pozovi nas</p>
-                            {/* Zakažite izlazak na teren poziv */}
-                        </a>
-                    </div>
-                </div>
+            <div className="form-actions">
+                <Contact />
                 <Button
                     variant="primary"
                     onClick={() => {
--- src/ui/moleculs/Table.molecul.tsx
+++ src/ui/moleculs/Table.molecul.tsx
@@ -3,9 +3,7 @@ import { PDFDownloadLink } from "@react-pdf/renderer";
 import { Button } from "../components/atoms/Button.atom";
 import Statement from "../components/containers/Statement.container";
 import StatementPDF from "../components/containers/StatementPdf.container";
-
-import email from "../../assets/email.svg";
-import tel from "../../assets/tel.svg";
+import Contact from "../components/atoms/Contact.atom";
 
 import "../styles/statement.scss";
 
@@ -19,63 +17,8 @@ export default function Table({ setPage }: { setPage: CallableFunction }) {
             </PDFViewer> */}
 
             <Statement />
-            <div className="schedule">
-                <p
-                    style={{
-                        fontWeight: "800",
-                        textAlign: "left",
-                        justifyContent: "space-between",
-                        fontSize: "clamp(1.125rem, 3vw, 1.2rem)",
-                        marginRight: "auto",
-                    }}
-                >
-                    Zakažite izlazak na teren
-                </p>
-                <div style={{ display: "flex", justifyContent: "flex-start" }}>
-                    <a
-                        style={{
-                            padding: ".5rem 1rem",
-                            display: "flex",
-                            alignContent: "center",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            gap: ".5rem",
-                        }}
-                        href="mailto:info@deking.com"
-                        // className="button outline mailto"
-                    >
-                        <img style={{ width: "1.25rem" }} src={email} alt="mail" />
-                        <p>Posalji email</p>
-                    </a>
-                    <a
-                        href="tel:+38162209207"
-                        style={{
-                            padding: ".5rem 1rem",
-                            display: "flex",
-                            alignContent: "center",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            gap: ".5rem",
-                        }}
-                        // className="button outline tel"
-                    >
-                        <img style={{ width: "1.25rem" }} src={tel} alt="tel" />
-                        <p>Pozovi nas</p>
-                        {/* Zakažite izlazak na teren poziv */}
-                    </a>
-                </div>
-            </div>
-
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    gap: "1rem",
-                    width: "100%",
-                    marginTop: "1rem",
-                    alignItems: "center",
-                }}
-            >
+            <Contact />
+            <div className="statement-actions">
                 <Button
                     variant="secondary"
                     onClick={() => {
